c73866d68fff17aeb30738af0b120235cc2ca2c3
Jacob Massengill <jacob_massengill@yahoo.com>
2016-12-14 11:11:25 -0500

    second pass at persistent navbar

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,13 @@
-if(!window.nav_open)
-    window.nav_open = false;
 function show_nav(){
     var nav = document.getElementsByClassName("nav")[0];
-    if(window.nav_open){
+    var nav_open = localStorage.getItem("nav_open");
+    console.log(nav_open); 
+    if(nav_open == "1"){
         nav.style.width = "1.5em";
         nav.style.height= "20px";
-        window.nav_open = false;
     }else{
         nav.style.width = "15%";
         nav.style.height= "100%";
-        window.nav_open = true;
     }
-
+    localStorage.setItem("nav_open", nav_open ^ 1);
 }
-show_nav();
