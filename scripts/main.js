dd50820b8e314c23affb97437df4b6f2e9262e79
Jacob Massengill <jacob_massengill@yahoo.com>
2016-12-14 09:03:53 -0500

    nav now hides completely

--- scripts/main.js
+++ scripts/main.js
@@ -2,10 +2,12 @@ nav_open = false;
 function show_nav(){
     var nav = document.getElementsByClassName("nav")[0];
     if(nav_open){
-        nav.style.width = "2%";
+        nav.style.width = "1.5em";
+        nav.style.height= "20px";
         nav_open = false;
     }else{
         nav.style.width = "15%";
+        nav.style.height= "100%";
         nav_open = true;
     }
 }
