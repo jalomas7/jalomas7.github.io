f0f60956330c22e21246cd39fa412361addd4d0c
Jacob Massengill <jacob_massengill@yahoo.com>
2016-12-14 10:42:21 -0500

    first pass at persistent navbar

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,16 @@
-var nav_open;
-if($.cookie("nav_open") in window){
-   nav_open = $.cookie("nav_open");
-}else{
-    nav_open = false;
-}
+if(!window.nav_open)
+    window.nav_open = false;
 function show_nav(){
     var nav = document.getElementsByClassName("nav")[0];
-    if(nav_open){
+    if(window.nav_open){
         nav.style.width = "1.5em";
         nav.style.height= "20px";
-        nav_open = false;
+        window.nav_open = false;
     }else{
         nav.style.width = "15%";
         nav.style.height= "100%";
-        nav_open = true;
+        window.nav_open = true;
     }
 
-    $.cookie("nav_open", nav_open);
 }
+show_nav();
