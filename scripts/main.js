763c655b15b64a3b554767c7591f9b50e3c5c373
Jacob Massengill <jacob_massengill@yahoo.com>
2016-12-16 10:03:23 -0500

    nav now has persistent state

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,14 @@
-function show_nav(){
+function show_nav(toggle){
     var nav = document.getElementsByClassName("nav")[0];
     var nav_open = localStorage.getItem("nav_open");
     console.log(nav_open); 
-    if(nav_open == "1"){
+    if(nav_open == "1" ^ toggle){
         nav.style.width = "1.5em";
         nav.style.height= "20px";
     }else{
         nav.style.width = "15%";
         nav.style.height= "100%";
     }
-    localStorage.setItem("nav_open", nav_open ^ 1);
+    if(toggle)
+        localStorage.setItem("nav_open", nav_open ^ 1);
 }
