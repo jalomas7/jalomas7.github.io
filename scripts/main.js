201a41d591f1795c49fea1f6e59fd8174e82a53e
Jacob Massengill <jacob_massengill@yahoo.com>
2016-12-16 10:37:28 -0500

    main content moves with nav

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,17 @@
 //sets the nav state as open or closed. persistent on page reload
 function show_nav(toggle){
     var nav = document.getElementsByClassName("nav")[0];
+    var content = document.getElementsByClassName("main_content")[0];
     var nav_open = localStorage.getItem("nav_open");
     console.log(nav_open); 
     if(nav_open == "1" ^ toggle){
         nav.style.width = "1.5em";
         nav.style.height= "20px";
+        content.style.left = "5%";
     }else{
         nav.style.width = "15%";
         nav.style.height= "100%";
+        content.style.left = "15%";
     }
     if(toggle)
         localStorage.setItem("nav_open", nav_open ^ 1);
