8595ed337bf0d5577355778dd8c5711170843f67
Jacob Massengill <jacob_massengill@yahoo.com>
2017-01-12 21:08:42 -0500

    added support for dropdown-lists in nav menu

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,16 @@
-//sets the nav state as open or closed. persistent on page reload
-function show_nav(toggle){
+//sets the nav state as open or closed.
+var nav_open = 1;
+function show_nav(){
     var nav = document.getElementsByClassName("nav")[0];
     var content = document.getElementsByClassName("main_content")[0];
-    var nav_open = localStorage.getItem("nav_open");
-    console.log(nav_open); 
-    if(nav_open == "1" ^ toggle){
-        nav.style.width = "1.7em";
-        nav.style.height= "25px";
+    if(nav_open){
+        nav.style.width = "1.9em";
+        nav.style.height= "30px";
         content.style.left = "5%";
     }else{
         nav.style.width = "15%";
         nav.style.height= "100%";
         content.style.left = "15%";
     }
-    if(toggle)
-        localStorage.setItem("nav_open", nav_open ^ 1);
+    nav_open ^= 1;
 }
