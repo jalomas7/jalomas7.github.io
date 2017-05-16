f91f5884c86ccc620fa395d670a238a291743f06
Jacob Massengill <jacob_massengill@yahoo.com>
2017-05-15 22:23:36 -0400

    nav retracts when clicked off of while open

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,8 @@
+function setup(){
+    show_nav();
+    document.body.addEventListener('click', check_nav, true);
+}
+
 //sets the nav state as open or closed.
 var nav_open = 1;
 function show_nav(){
@@ -5,7 +10,7 @@ function show_nav(){
     var nav_button = document.getElementById("nav_open");
     var content = document.getElementsByClassName("main_content")[0];
     if(nav_open){
-        nav.style.width = "1.9em";
+        nav.style.width = "2em";
         nav.style.height= "30px";
         content.style.left = "5%";
         nav.style.overflow = "hidden";
@@ -22,4 +27,10 @@ function show_nav(){
         nav.style.boxShadow = "0px 0px 2px 1px #202b35";
     }
     nav_open ^= 1;
+}
+
+function check_nav(evt) {
+    if(nav_open && evt.clientX > $(window).width()*0.15){
+        show_nav(); 
+    }
 }
