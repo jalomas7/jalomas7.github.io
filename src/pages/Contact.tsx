bd49a74b9fb5efbabbd26d7bf720525f7b29af67
Jacob Massengill <jacob_massengill@yahoo.com>
2025-01-09 09:23:08 -0600

    center text on contact page

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,13 +1,18 @@
+import newStyled from "@emotion/styled"
 import React from "react"
+
+const Container = newStyled.div`
+    text-align: center;
+`
 
 export const Contact: React.FunctionComponent = () => {
     return (
-        <div>
+        <Container>
             <h1>Contact me:</h1>
             <div>
                 <p>Phone: 615-917-9715</p>
                 <p>Email: jacobmassengill10@gmail.com</p>
             </div>
-        </div>
+        </Container>
     )
 }
